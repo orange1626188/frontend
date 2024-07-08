f1492f209050fe6f3cd8629ba99730d43c151ef3
orange1626188 <orange162618@gmail.com>
2024-07-08 13:32:26 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,133 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+    <>
+ <div className="container-fluid">
+<div className="row">
+  <div className="col-md-12 text-center p-0">
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
           </a>
-        </div>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </div>
+</div>
+
+<div className="row">
+  <div className="col-md-12 text-center p-0">
+  <div id="carouselExample" className="carousel slide">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://cdn.onemars.net/sites/whiskas_th_r81SA_mwh5/image/cat-breeds-15_1920x670_1652359490366.jpeg" className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="https://cdn.onemars.net/sites/whiskas_th_r81SA_mwh5/image/cat-breeds-15_1920x670_1652359490366.jpeg" className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="https://cdn.onemars.net/sites/whiskas_th_r81SA_mwh5/image/cat-breeds-15_1920x670_1652359490366.jpeg" className="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+  </div>
+</div>
+ิ<br></br>
+
+<div className="row">
+  <div className="col-sm-4 mb-3 mb-sm-0">
+    <div className="card">
+      <div className="card-body">
+        <img src="https://www.purina.co.th/sites/default/files/styles/ttt_image_510/public/2021-01/Persian%20Long%20Hair.1.jpg?itok=vxUPA23O"className="card-mig-top" alt="..." />
+        <h5 className="card-title">แมวเปอร์เซีย</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    </div>
+  </div>
+  <div className="col-sm-4">
+    <div className="card">
+      <div className="card-body">
+      <img src="https://www.purina.co.th/sites/default/files/styles/ttt_image_510/public/2021-01/Scottish%20Fold.2.jpg?itok=1LUhD8ze"className="card-mig-top" alt="..." />
+        <h5 className="card-title">แมวสก๊อตติสโฟลด์</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    </div>
+  </div>
+  <div className="col-sm-4 mb-3 mb-sm-0">
+    <div className="card">
+      <div className="card-body">
+      <img src="https://yok51120.wordpress.com/wp-content/uploads/2014/12/maine-coon-03.jpg"className="card-mig-top" alt="..." />
+        <h5 className="card-title">แมวเมนคูน</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
       </div>
-    </main>
+    </div>
+  </div>
+</div>
+<br></br>
+<div className="row">
+          <div className="col-md-12 text-center bg-white p-5">
+          <div className="container">
+  <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p className="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
+    <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg className="bi me-2" width={40} height={32}><use xlinkHref="#bootstrap" /></svg>
+    </a>
+    <ul className="nav col-md-4 justify-content-end">
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+  </footer>
+</div>
+
+          </div>
+</div>
+</div>
+    </>
   );
 }
